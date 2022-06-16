7905da1e421e3e0f69fbc41dc8ee635bd645dd5f
Riciéri Andrei dos Santos <ricieris@furb.br>
2022-06-15 21:15:25 -0300

    indo, só indo

--- POO/Lanchonete/src/classes/lanches/Lanche.java
+++ POO/Lanchonete/src/classes/lanches/Lanche.java
@@ -11,31 +11,28 @@ public abstract class Lanche {
     public void adicionarIngrediente(String ingrediente) {
         this.ingredientes.add(ingrediente);
     }
+
     public abstract void mostrarDetalhesComanda();
 
-    public abstract void montarDetalhesLanche (Scanner scan);
+    public abstract void montarDetalhesLanche(Scanner in);
 
-
-
-
-    //Getters & Setters
     public ArrayList<String> getIngredientes() {
         return ingredientes;
+    }
+
+    public double getValor() {
+        return valor;
     }
 
     public void setValor(double valor) {
         this.valor = valor;
     }
 
-    public double getValor() {
-        return this.valor;
+    public String getTipo() {
+        return tipo;
     }
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
-    }
-
-    public String getTipo() {
-        return this.tipo;
     }
 }
