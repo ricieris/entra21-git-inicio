48d1b53f5d823159714501a68aaa988a7d4e9f71
Riciéri Andrei dos Santos <ricieris@furb.br>
2022-06-27 20:38:59 -0300

    onibus certo

--- CompanhiaDeViagem/Viagem/src/Main.java
+++ CompanhiaDeViagem/Viagem/src/Main.java
@@ -1,3 +1,16 @@
-public class Main {
+import classes.Assento;
+import classes.AssentoOnibus;
+import classes.veiculo.Onibus;
 
+public class Main {
+    public static void main(String[] args) {
+        Onibus o = new Onibus(7);
+        o.getAssento("1").setOcupado(true);
+        o.getAssento("6").setOcupado(true);
+        o.getAssento("7").setOcupado(true);
+        o.getAssento("8").setOcupado(true);
+        o.getAssento("15").setOcupado(true);
+        o.getAssento("16").setOcupado(true);
+        o.mostrarAssentos();
+    }
 }
