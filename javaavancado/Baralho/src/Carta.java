8d70404c37eb0c593d3e0097114d0f2075523f63
Riciéri Andrei dos Santos <ricieris@furb.br>
2022-06-29 21:43:08 -0300

    cartas

---
+++ javaavancado/Baralho/src/Carta.java
@@ -0,0 +1,22 @@
+public class Carta {
+    private Naipe naipe;
+    private ValorCarta valor;
+
+
+    // Getters & Setters
+    public Naipe getNaipe() {
+        return naipe;
+    }
+
+    public void setNaipe(Naipe naipe) {
+        this.naipe = naipe;
+    }
+
+    public ValorCarta getValor() {
+        return valor;
+    }
+
+    public void setValor(ValorCarta valor) {
+        this.valor = valor;
+    }
+}
---
+++ javaavancado/Baralho/src/ValorCarta.java
@@ -0,0 +1,26 @@
+public enum ValorCarta {
+    AS("A"),
+    DOIS("2"),
+    TRES("3"),
+    QUATRO("4"),
+    CINCO("5"),
+    SEIS("6"),
+    SETE("7"),
+    OITO("8"),
+    NOVE("9"),
+    DEZ("10"),
+    VALETE("J"),
+    DAMA("Q"),
+    REI("K");
+
+    private final String valor;
+
+    // Getters & Setters
+    ValorCarta(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return this.valor;
+    }
+}
