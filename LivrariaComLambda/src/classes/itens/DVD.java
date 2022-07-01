bb5aae23d12789de9869adfd659a16e9f07594ad
Riciéri Andrei dos Santos <ricieris@furb.br>
2022-07-01 20:58:56 -0300

    ajustes

---
+++ LivrariaComLambda/src/classes/itens/DVD.java
@@ -0,0 +1,50 @@
+package classes.itens;
+
+import java.util.Scanner;
+
+public class DVD extends Item {
+    private String diretor;
+    private double duracao;
+    private int anoLancamento;
+
+    // Getters & Setters
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    @Override
+    public void montarDetalhes(Scanner in) {
+        System.out.println("Informe o diretor: ");
+        this.setDiretor(in.nextLine());
+        System.out.println("Informe o ano de lançamento: ");
+        this.setAnoLancamento(in.nextInt());
+        System.out.println("Informe a duração");
+        this.setDuracao(in.nextDouble());
+    }
+
+    @Override
+    public void mostrarDetalhes() {
+
+    }
+}
